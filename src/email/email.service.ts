c46121ab24b767fb25532bd60dd5143eb3a7a14a
fahad-6sensehq <fahad@6sensehq.com>
2025-02-10 21:57:09 +0600

    minor update

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class EmailService {
     sendEmail() {
-        return 'Send email';
+        return { response: 'Send email' };
     }
 }
